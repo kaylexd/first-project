6c7728522ab6031652b2f9f2aea30aacb7061bc8
kaylexd <kaylexd6@gmail.com>
2024-07-13 10:58:15 +0800

    Hello World

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+
+      <p>
+        Hello there.
+        <br /> <h2>What is React?</h2>
+      </p>
+      <p>
+        React is a free and open-source front-end JavaScript library for
+        building user interfaces based on components by Facebook Inc. It is
+        maintained by Meta and a community of individual developers and
+        companies. React can be used to develop single-page, mobile, or
+        server-rendered applications with frameworks like Next.js.
+      </p>
+      <p></p>
+      <ol>
+        <li>Kayle Louise Lumapay</li>
+        <li>Jefferson Cansancio</li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
